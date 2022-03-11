8645756956d7a3ce7027df0640abfcf7de605479
Yohannes Tadesse <yohatad000@gmail.com>
2022-03-11 13:31:47 +0300

    fix bug

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit after compare it with 5
+ * Descibe: Get a random number and check its last digit after compare it with 5
  * Return: 0
  */
 int main(void)
