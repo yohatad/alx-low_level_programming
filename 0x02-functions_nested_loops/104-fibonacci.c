2a5c36fb187bbf18ad8fa5bc1ed15cba328b8c8d
Yohannes Tadesse <yohatad000@gmail.com>
2022-03-16 01:04:47 +0300

    fix bug

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 97)
+	while (i < 96)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 96)
+		if (i < 95)
 			printf(", ");
 		i++;
 	}
