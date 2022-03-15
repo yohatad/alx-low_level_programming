636648d956228e8cb96b1f618128f887a538d5b9
Yohannes Tadesse <yohatad000@gmail.com>
2022-03-16 01:00:04 +0300

    fix error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the 9 table
- * @n: n times table
- *  Description: prints all the nine table
- * Return: Always (0).
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
 
 void print_times_table(int n)
 {
-    int i, j, product;
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            if (j == 0)
-            {
-                _putchar(48);
-                continue;
-            }
-            product = i * j;
-            _putchar(',');
-            _putchar(' ');
-            if (product >= 10)
-            {
-                _putchar(product / 10 + 48);
-                _putchar(product % 10 + 48);
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar(product + 48);
-            }
-        }
-        _putchar('\n');
-    }
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
+	{
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	a++;
+}
 }
